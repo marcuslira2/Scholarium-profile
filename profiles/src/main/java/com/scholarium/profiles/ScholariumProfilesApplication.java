b06deba24cc741720ae1b63eeeaa7763718562a0
Marcus Lira <marcuslira57@gmail.com>
2024-09-19 20:46:07 -0300

    Feature: Inicial commit scholarium profile

---
+++ profiles/src/main/java/com/scholarium/profiles/ScholariumProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.scholarium.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholariumProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholariumProfilesApplication.class, args);
+	}
+
+}
